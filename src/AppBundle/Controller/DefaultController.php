7db0813363a119ff53ba15a86fa3ebbbb03769cb
Berny Cantos <be@rny.cc>
2017-02-25 19:23:31 +0000

    move functionality to controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,142 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/register", name="account_register")
      */
-    public function indexAction(Request $request)
+    public function registerAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        $email = '';
+        $name = '';
+        $errors = [];
+        if ($request->getMethod() === Request::METHOD_POST) {
+
+            $email = $request->request->get('email');
+            if (empty($email)) {
+                $errors['email'][] = 'Email field must not be empty';
+
+            } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors['email'][] = 'Email field must be a valid email';
+
+            } elseif ($this->accountAlreadyExistsWithEmail($email)) {
+                $errors['email'][] = 'Email is already registered';
+            }
+
+            $name = $request->request->get('name');
+            if (empty($name)) {
+                $errors['name'][] = 'Name must not be empty';
+            }
+
+            $password = $request->request->get('password');
+            if (empty($password)) {
+                $errors['password'][] = 'Password cannot be empty';
+
+            } elseif (!$this->passwordIsSafe($password)) {
+                $errors['password'][] = 'Password is not safe enough';
+            }
+
+            $passwordAgain = $request->request->get('password-again');
+            if ($password !== $passwordAgain) {
+                $errors['password-again'][] = 'Both passwords must match';
+            }
+
+            if (empty($errors)) {
+                $accountId = $this->doRegisterAccount($email, $password, $name);
+
+                return $this->redirectToRoute('account_welcome', [
+                    'account-id' => $accountId,
+                ]);
+            }
+        }
+
+        return $this->render('default/register.html.twig', [
+            'errors' => $errors,
+            'email' => $email,
+            'name' => $name,
         ]);
+    }
+
+    /**
+     * @Route("/welcome", name="account_welcome")
+     */
+    public function welcomeAction(Request $request)
+    {
+        $accountId = $request->query->get('account-id');
+        $account = $this->findAccount($accountId);
+
+        return $this->render('default/welcome.html.twig', [
+            'name' => $account['name'],
+        ]);
+    }
+
+    private function findAccount($accountId)
+    {
+        $connection = $this->databaseConnection();
+
+        $statement = $connection->query(sprintf(
+            'SELECT * FROM accounts WHERE id = %d LIMIT 1',
+            $accountId
+        ));
+
+        return $statement->fetch();
+    }
+
+    private function accountAlreadyExistsWithEmail($email)
+    {
+        $connection = $this->databaseConnection();
+
+        $statement = $connection->query(sprintf(
+            'SELECT id FROM accounts WHERE email = "%s" LIMIT 1',
+            $email
+        ));
+
+        return $statement->rowCount() > 0;
+    }
+
+    private function doRegisterAccount($email, $password, $name)
+    {
+        $connection = $this->databaseConnection();
+
+        $connection->exec(sprintf(
+            'INSERT INTO accounts SET email="%s", name="%s", password="%s"',
+            $email,
+            $name,
+            $password
+        ));
+
+        $id = $connection->lastInsertId();
+
+        $this->sendConfirmationEmail($id, $email, $name);
+
+        return $id;
+    }
+
+    private function passwordIsSafe($password)
+    {
+        return strlen($password) >= 4;
+    }
+
+    private function sendConfirmationEmail($id, $email, $name)
+    {
+        $subject = sprintf(
+            'Thanks for registering, %s',
+            $name
+        );
+
+        $welcomeUrl = $this->generateUrl('account_welcome', [
+            'account-id' => $id,
+        ]);
+
+        $message = sprintf(
+            'Dear %s, we are glad you register in our website, please do visit %s to follow.',
+            $name,
+            $welcomeUrl
+        );
+
+        mail($email, $subject, $message);
+    }
+
+    private function databaseConnection()
+    {
+        return new \PDO('mysql:host=127.0.0.1;dbname=profile', 'root', 'root');
     }
 }
